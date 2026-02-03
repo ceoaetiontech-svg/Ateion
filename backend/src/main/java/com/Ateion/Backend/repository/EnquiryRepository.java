f5a55f0d85fc0d6f54f30ca0288413930e761b3a
Anjali569 <jhaa34563@gmail.com>
2026-02-03 15:34:51 +0530

    Revert "Reapply "Reapply "Merge branch 'backend' of https://github.com/ceoaetiontech-svg/Ateion into backend"""
    This reverts commit 7fc07df8bcdcce7aae7efa648f56eca87364e5ea.

--- backend/src/main/java/com/Ateion/Backend/repository/EnquiryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Ateion.Backend.repository;
-
-import com.Ateion.Backend.entity.Enquiry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnquiryRepository extends JpaRepository<Enquiry, Long> {
-}
