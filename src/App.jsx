d1d6c5938edee175d44346c1990cd3b0a511e05d
Neha Lokhande <lokhandeneha183@gmail.com>
2026-03-13 11:41:37 +0530

    Added my code - Sakshi Kasurkar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="text-group">
+        <h1 className="title">Education Systems Are Fighting the Last War</h1>
+        <p className="description">
+          Highlighting facts like 40–70% of graduates lacking job readiness 
+          and 44–50% of workforce skills changing in 5 years.
+        </p>
+      </div>
+      <div className="rectangle"></div>
+    </div>
+  );
+}
+
+export default App;
